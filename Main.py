3a88a65bb34064fe884ca334277efb9cab7469ea
Luca Angeloni <lucaangeloni@gmail.com>
2023-09-20 16:08:33 -0300

    V 0.1

--- Main.py
+++ Main.py
@@ -2,12 +2,32 @@ import tkinter
 
 vent = tkinter.Tk()
 vent.title("Aula")
-vent.attributes('-fullscreen', True)
+vent.attributes()
+vent.geometry("500x300")
+vent['bg'] = 'sky blue'
 
-menu_archivo = tk.Menu(vent, tearoff=0)
-menu_editar = tk.Menu(vent, tearoff=0)
-menu_ayuda = tk.Menu(vent, tearoff=0)
+NumAulas = 0
 
-box1 = tkinter.Entry(vent)
-box1.pack()
+NumeroDeAulas = tkinter.Label(vent, background='sky blue', font='calibri')
+NumeroDeAulas.place(x=100, y=50)
+NumeroDeAulas.pack()
+
+NombreAula = tkinter.Entry(text="nombre del aula ")
+
+def NuevaAula():
+    global NumAulas
+    NumAulas = NumAulas + 1
+    NumeroDeAulas.config(text = NumAulas)
+
+    
+    
+MenuPrincipal = tkinter.Menu(vent, background='purple', fg='white')
+archivo = tkinter.Menu(MenuPrincipal, tearoff=False)
+
+archivo.add_command(label="Nueva Aula", command=NuevaAula, background='sky blue')
+archivo.add_separator()
+archivo.add_command(label="Salir", command=vent.quit, background='red')
+
+MenuPrincipal.add_cascade(menu=archivo, label="Archivo")
+vent.config(menu=MenuPrincipal)
 vent.mainloop()
