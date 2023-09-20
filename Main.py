2fea73449c4ff73fc1d12c07c4f094ca1a16fcb3
Luca Angeloni <lucaangeloni@gmail.com>
2023-09-20 14:51:11 -0300

    V0.1 Bandejas

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 import tkinter
 
 vent = tkinter.Tk()
-vent.title("Base de datos")
+vent.title("Aula")
 vent.attributes('-fullscreen', True)
+
+menu_archivo = tk.Menu(vent, tearoff=0)
+menu_editar = tk.Menu(vent, tearoff=0)
+menu_ayuda = tk.Menu(vent, tearoff=0)
 
 box1 = tkinter.Entry(vent)
 box1.pack()
