ebf6e93cbcf66940bb8ed50b86d939010e809f53
Luca Angeloni <angeloniluca07@gmail.com>
2023-11-04 01:03:12 -0300

    V0.1

--- pyautogui/Main.py
+++ pyautogui/Main.py
@@ -1,9 +1,15 @@
 import pyautogui
-distance = 200
-while distance > 0:
-    pyautogui.drag(distance, 0, duration=0.1)   
-    distance -= 5
-    pyautogui.drag(0, distance, duration=0.1)   
-    pyautogui.drag(-distance, 0, duration=0.1)  
-    distance -= 5
-    pyautogui.drag(0, -distance, duration=0.1)
+#distance = 200
+#while distance > 0:
+#    pyautogui.drag(distance, 0,duration=0.2)   
+#    distance -= 5 
+#    pyautogui.drag(0, distance, duration=0.2)   
+#    pyautogui.drag(-distance, 0, duration=0.2)  
+#    distance -= 5
+#    pyautogui.drag(0, -distance, duration=0.2)
+
+r= None 
+while r is None:
+    Butt1 = pyautogui.locateAllOnScreen('C:\Temp\Git\Gitbash\Main\pyautogui\imagen1.png', grayscale=False)
+    pyautogui.click(Butt1)
+print (r)
